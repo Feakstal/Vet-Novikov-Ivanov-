4b1689522c68f9f2876c992c0b456f611b9936e3
Feakstal <74710033+Feakstal@users.noreply.github.com>
2021-11-16 16:44:07 +0300

    fist♂

---
+++ changes/ClientsPage.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Navigation;
+using Vet.DataBase;
+using Vet.Views;
+
+namespace Vet.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientsPage.xaml
+    /// </summary>
+    public partial class ClientsPage : Page
+    {
+        public static Entities Entities = new Entities();
+        public Role role;
+        public ClientsPage()
+        {
+            InitializeComponent();
+            ClientsGrid.ItemsSource = Entities.Client.ToList();
+            role = Entities.Role.Find(AuthWindow.authUser.Role.IDRole);
+            if (role.RoleName.Equals("Врач"))
+            {
+                btnDelete.Visibility = Visibility.Collapsed;
+                btnSave.Visibility = Visibility.Collapsed;
+                btnAddClientWindow.Visibility = Visibility.Collapsed;
+                ClientsGrid.CanUserAddRows = false;
+                ClientsGrid.IsReadOnly = true;
+            }
+        }
+
+        private void btnGoBack_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService ns = this.NavigationService;
+            ns.Navigate(new MainPage());
+        }
+
+        class ClientsTable
+        {
+            public ClientsTable(int IDClient, string Surname, string Name, string FatherName, string Phone, string PassSerial, string PassNumber, int IDPatient, bool IsDeleted)
+            {
+                this.IDClient = IDClient;
+                this.Surname = Surname;
+                this.Name = Name;
+                this.FatherName = FatherName;
+                this.Phone = Phone;
+                this.PassSerial = PassSerial;
+                this.PassNumber = PassNumber;
+                this.IDPatient = IDPatient;
+                this.IsDeleted = IsDeleted;
+            }
+            public int IDClient { get; set; }
+            public string Surname { get; set; }
+            public string Name { get; set; }
+            public string FatherName { get; set; }
+            public string Phone { get; set; }
+            public string PassSerial { get; set; }
+            public string PassNumber { get; set; }
+            public int IDPatient { get; set; }
+            public bool IsDeleted { get; set; }
+        }
+
+        private void tboxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string filter = tboxSearch.Text;
+            ICollectionView viewSource = CollectionViewSource.GetDefaultView(ClientsGrid.ItemsSource);
+            if (filter == "") viewSource.Filter = null;
+            else
+            {
+                viewSource.Filter = SN =>
+                {
+                    Client p = SN as Client;
+                    return p.Surname.ToString().ToLower().Contains(filter);
+                };
+                ClientsGrid.ItemsSource = viewSource;
+            }
+        }
+        private void btnAddClientWindow_Click(object sender, RoutedEventArgs e)
+        {
+            AddClientWindow addClientWindow = new AddClientWindow();
+            addClientWindow.ShowDialog();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Вы хотите удалить данную запись?", "Удаление клиента", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                if (ClientsGrid.SelectedItem is Client client)
+                {
+                    client.IsDeleted = true;
+                    Entities.SaveChanges();
+                    MessageBox.Show("Запись успешно удалена", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                    ClientsGrid.ItemsSource = Entities.Client.ToList();
+                }
+                else
+                {
+                    MessageBox.Show("Вы не выбрали пользователя из списка", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            Entities.SaveChanges();
+            MessageBox.Show("Данные успешно сохранены.", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
